4bd8be9b31450f06f9433e2c78175ed3212018f5
堀川浩二 <>
2019-07-02 19:40:23 +0800

    app.phpの変更

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -25,7 +25,7 @@
 
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-dark bg-black shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name') }}
